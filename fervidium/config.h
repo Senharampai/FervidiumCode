6b8472406e7b332cacb6d971e1246f56ce1982a0
Senharampai <62100161+Senharampai@users.noreply.github.com>
2023-12-05 20:46:40 +0100

    Add files via upload

---
+++ fervidium/config.h
@@ -0,0 +1,26 @@
+// Copyright 2023 Senharampai (@Senharampai)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define EE_HANDS
+#define MASTER_LEFT
+#define SELECT_SOFT_SERIAL_SPEED 0
+
+#define WS2812_DI_PIN GP25
+#define RGBLED_NUM 30
+#define RGBLED_SPLIT { 15, 15 }
+
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_GRB
+
+#define RGBLIGHT_MODE_BREATHING 1
+#define RGBLIGHT_MODE_RAINBOW_MOOD 1
+#define RGBLIGHT_MODE_RAINBOW_SWIRL 1
+
+#define RGBLIGHT_HUE_STEP 12
+#define RGBLIGHT_SAT_STEP 25
+#define RGBLIGHT_VAL_STEP 12
+
+#define ENCODERS_PAD_A { GP21 }
+#define ENCODERS_PAD_B { GP23 }
+#define ENCODER_RESOLUTIONS_LEFT { 2 }
+#define ENCODER_RESOLUTIONS_RIGHT { 4 }
